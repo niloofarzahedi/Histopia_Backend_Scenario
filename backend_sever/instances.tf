98e899cbbd14dce85856a7cf0c84a0fc5b2fc53b
niloofarzahedi <niloofarzahedi75@gmail.com>
2024-07-12 17:16:59 +0100

    instance config

---
+++ backend_sever/instances.tf
@@ -0,0 +1,33 @@
+#instance ami data source
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+#aws instance
+
+resource "aws_instance" "HistopiaBackend" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.example.id
+  user_data     = <<EOF
+#! /bin/bash
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><head><title>Taco Team Server1</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+EOF
+  tags = {
+    Name = "tf-example"
+  }
+}
